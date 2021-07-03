5c9e5fd3621d00807a2f3bfbcbf39be8f01385a5
NoobMaester <joshstilez@gmail.com>
2021-07-03 02:17:47 +0200

    responsiveness

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//DECLARING VARIABLES
-var getPost = document.getElementById('btn');
-
 
 //FETCHING USERS FROM API
 const getUser = async () => {
@@ -14,42 +11,39 @@ const getUser = async () => {
     data.forEach((user, index) => {
         let id = index + 1;
         html +=`
-        <div id="user">
-            <div class="text">
-                <h4>${user.name}</h4>
-                <p>${user.email}</p>
+        <div class="users">
+            <h5>User ${user.id}</h5>
+            <div id="user">
+                <div class="text">
+                    <h4>${user.name}</h4>
+                    <p>${user.email}</p>
+                </div>
+                <a id='btn' onclick = "viewPost(${id})" >Get User's Posts</a>
             </div>
-            <button id='btn' onclick = "viewPost(${id})" >Get User's Posts</button>
         </div>
         `;
         document.getElementById('getRequest').innerHTML = html;
-        
     });
 };
 getUser();
 
-const viewPost = (id) => {
-    
-    window.location.href = 'post.html';
-    console.log(`clicked ${id}`);
-
-    
-}
-const postDisplay = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users/8/posts`);
+const viewPost = async (id) => {
+    console.log(`${id} clicked`);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`);
     const posts = await response.json();
     console.log(posts);
+    
     let html1 = "";
 
     posts.forEach(post => {
     html1 += `
     <div class="post">
+        <h5>Article ${post.id}</5>
         <h3>${post.title}</h3>
         <p>${post.body}</p>
     </div>
     `;
-    });
-    document.getElementById("userPost").innerHTML = html1;
 
+    });
+    document.getElementById("getRequest").innerHTML = html1;    
 }
-postDisplay()
